24731a74c82b48f9f327dde657850392d72aaa00
Isaac Moon <dev.chanyongmoon@gmail.com>
2025-09-25 11:21:20 +0900

    change entry points

--- charts/k8s/entrypoint.sh
+++ charts/k8s/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 echo "Sourcing environment variables from /app/env/config.env"
-source /app/env/config.env
+
+set -a
+source /app/env/.env
+set +a
+
 
 echo "Starting the application..."
 pnpm start
